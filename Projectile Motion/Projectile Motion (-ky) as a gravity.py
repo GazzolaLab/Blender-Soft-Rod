827bedb881e081ed2ce28a485fa16c1c52f95b0f
adikorlahalli <115048691+adikorlahalli@users.noreply.github.com>
2023-12-01 03:33:05 -0600

    Add files via upload

---
+++ Projectile Motion/Projectile Motion (-ky) as a gravity.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+v0 = 25
+
+x = np.array([0, v0])
+dt = 10**(-3)
+ 
+def f(x):
+    k = 5
+    y, v= x[0], x[1]
+    return np.array([v, (-1) * k * y])
+
+time = np.arange(0, 5, dt)
+
+pos = [x[0]]
+for k, t in enumerate(time[:-1]):
+    x = x + f(x) * dt
+    pos.append(x[0])
+
+plt.plot(time, pos, '-b', label='Position (y) part a')
+plt.xlabel('Time (t)')
+plt.ylabel('Position (y)')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
