0bd1fcb46a108be40256aad95693fed3f4eab2e8
Seung Hyun Kim <skim449@illinois.edu>
2024-05-19 15:55:49 -0500

    add skeleton to make blender soft-rod package

---
+++ src/bsr/geometry.py
@@ -0,0 +1,64 @@
+import bpy
+import numpy as np
+import colorsys
+
+
+class Sphere:
+    def __init__(self, location, radius=0.005):
+        self.obj = self.create_sphere(location, radius)
+        
+    def create_sphere(self, location, radius):
+        bpy.ops.mesh.primitive_uv_sphere_add(radius=radius, location=location)
+        return bpy.context.active_object
+    
+    def update_position(self, location):
+        self.obj.location.z  = location[2]
+        self.obj.location.y = location[1]
+        self.obj.location.x = location[0]
+
+class Cylinder:
+    def __init__(self, pos1, pos2):
+        self.obj = self.create_cylinder(pos1, pos2)
+        self.mat = bpy.data.materials.new(name="cyl_mat")
+        self.obj.active_material = self.mat
+
+    def create_cylinder(self, pos1, pos2):
+        depth, center, angles = self.calc_cyl_orientation(pos1, pos2)
+        bpy.ops.mesh.primitive_cylinder_add(radius=0.005, depth=1, location=center)
+        cylinder = bpy.context.active_object
+        cylinder.rotation_euler = (0, angles[1], angles[0])
+        cylinder.scale[2] = depth
+        return cylinder
+
+    def calc_cyl_orientation(self, pos1, pos2):
+        pos1 = np.array(pos1)
+        pos2 = np.array(pos2)
+        depth = np.linalg.norm(pos2 - pos1)
+        dz =  pos2[2] - pos1[2]
+        dy = pos2[1] - pos1[1]
+        dx = pos2[0] - pos1[0]
+        center = (pos1 + pos2) / 2
+        phi = np.arctan2(dy, dx)
+        theta = np.arccos(dz/depth)
+        angles = np.array([phi, theta])
+        return depth, center, angles
+    
+    def update_position(self, pos1, pos2):
+        depth, center, angles = self.calc_cyl_orientation(pos1,pos2)
+        self.obj.location = (center[0],center[1],center[2])
+        self.obj.rotation_euler = (0, angles[1], angles[0])
+        self.obj.scale[2] = depth
+        
+        #computing deformation heat-map
+        max_def = 0.07
+        
+        h = -np.sqrt(self.obj.location[0]**2 + self.obj.location[2]**2)/max_def + 240/360
+        v = np.sqrt(self.obj.location[0]**2 + self.obj.location[2]**2)/max_def*0.5 + 0.5
+
+        r, g, b = colorsys.hsv_to_rgb(h, 1, v)
+        self.update_color(r,g,b,1)
+        
+    
+    def update_color(self, r,g,b,a):
+        self.mat.diffuse_color = (r,g,b,a)
+
---
+++ src/elastica_blender/rod_callback.py
@@ -0,0 +1,58 @@
+import bpy
+import numpy as np
+
+import numpy as np
+
+from elastica import CallBackBaseClass
+
+from bsr.geometry import Sphere, Cylinder
+
+
+class BlenderRodCallback(CallBackBaseClass):
+    """
+    Call back function for continuum snake
+    """
+
+    def __init__(self, step_skip: int, callback_params: dict):
+        CallBackBaseClass.__init__(self)
+        self.every = step_skip
+        self.bpy_objs = callback_params
+
+    def make_callback(self, system, time, current_step: int):
+        if current_step == 0:
+            self.build(system.position_collection)
+        elif current_step % self.every == 0:
+            self.update(
+                positions=system.position_collection,
+                time_step=current_step // self.every
+            )
+        else:
+            pass
+
+    def build(self, positions):
+        # New object
+        for j in range(positions.shape[-1]):
+            self.bpy_objs["sphere"].append(Sphere(positions[:, j]))
+        for j in range(positions.shape[-1]-1):
+            self.bpy_objs["cylinder"].append(
+                Cylinder(
+                    self.bpy_objs["sphere"][j].obj.location,
+                    self.bpy_objs["sphere"][j+1].obj.location
+                )
+            )
+
+    def update(self, positions, time_step):
+        # update all sphere and cylinder positions and write object to keyframe
+        for idx, sphere in enumerate(self.bpy_objs["sphere"]):
+            sphere.update_position(positions[:, idx])
+            sphere.obj.keyframe_insert(data_path="location", frame=time_step)
+
+        for idx, cylinder in enumerate(self.bpy_objs["cylinder"]):
+            cylinder.update_position(
+                self.bpy_objs["sphere"][idx].obj.location,
+                self.bpy_objs["sphere"][idx+1].obj.location
+            )
+            cylinder.obj.keyframe_insert(data_path="location", frame=time_step)
+            cylinder.obj.keyframe_insert(data_path="rotation_euler", frame=time_step)
+            cylinder.obj.keyframe_insert(data_path="scale", frame=time_step)
+            cylinder.mat.keyframe_insert(data_path="diffuse_color", frame=time_step)
