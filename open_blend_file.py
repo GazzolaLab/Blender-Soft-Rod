f7d93dd38af06263964acd0e4e148fbff2a8862c
Heng-Sheng Chang <hschang@Heng-Shengs-MacBook-Air.local>
2024-05-30 16:32:34 -0500

    Update open_blen_file.py

--- open_blend_file.py
+++ open_blend_file.py
@@ -1,9 +1,15 @@
 #This part will work either minimized like this or with the entire blender script
-
+import os
 import bpy
 
 #Opens file; Preset with previously saved file's filepath
-bpy.ops.wm.open_mainfile(filepath="/Users/rohitharish/Downloads/Blender Summer .blend files/rodsimvscode2.blend")   
+
+load_filepath = "3rodbr2_write.blend"
+load_filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), load_filepath)
+bpy.ops.wm.open_mainfile(filepath=load_filepath)   
+objects = bpy.context.scene.objects
+for obj in objects:
+    print(obj.name)
 
 #To open file in Blender, copy the line below into last line of terminal; Last part is the script file's name (Stored in my downloads section)
 
