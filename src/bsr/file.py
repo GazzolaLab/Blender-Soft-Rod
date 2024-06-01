933279d44486c3279253bd1b71f7d22d5f4d1295
Seung Hyun Kim <skim449@illinois.edu>
2024-05-31 22:54:17 -0500

    docs: add docstring

--- src/bsr/file.py
+++ src/bsr/file.py
@@ -1,9 +1,39 @@
+from pathlib import Path
+
 import bpy
 
 
-def save(path):
-    return path
+def save(path: Path | str) -> bool:
+    """
+    TODO: documentation: one sentence summary of what the function does.
+
+    Parameters
+    ----------
+    path : Path | str
+        File path to save the file to.
+
+    Returns
+    -------
+    bool
+        Whether the file was successfully saved.
+    """
+    raise NotImplementedError("Not implemented yet.")
+    return False
 
 
-def reload(path):
-    return path
+def reload(path: Path | str) -> bool:
+    """
+    TODO: documentation: one sentence summary of what the function does.
+
+    Parameters
+    ----------
+    path : Path | str
+        File path to reload the file from.
+
+    Returns
+    -------
+    bool
+        Whether the file was successfully reloaded.
+    """
+    raise NotImplementedError("Not implemented yet.")
+    return False
