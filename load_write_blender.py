8409ee948044d93f670ebc9151b84afe531fbd3d
Seung Hyun Kim <skim449@illinois.edu>
2024-06-06 06:08:38 +0900

    final formatting for PR

--- load_write_blender.py
+++ load_write_blender.py
@@ -1,8 +1,10 @@
 import os
-import numpy as np
+
 import bpy
+import numpy as np
 
 import bsr
+
 
 class PendulumBlender:
     def __init__(self, location, ball_radius):
@@ -11,29 +13,26 @@ class PendulumBlender:
 
         origin_location = np.array([0, 0, 0])
         # Add a sphere
-        self.add_sphere(
-            location=self.location, 
-            radius=self.ball_radius
-        )
+        self.add_sphere(location=self.location, radius=self.ball_radius)
         # Add a cylinder
         center, angle, length = PendulumBlender.calculate_cylinder_pose(
-            loc1=origin_location, 
-            loc2=self.location
+            loc1=origin_location, loc2=self.location
         )
         self.add_cylinder(
-            location=center, 
-            rotation=angle, 
-            depth=length, 
-            radius=0.1*self.ball_radius
+            location=center,
+            rotation=angle,
+            depth=length,
+            radius=0.1 * self.ball_radius,
         )
 
     def __repr__(self):
-        return f"Pendulum at {self.location} with ball radius {self.ball_radius}"
-    
+        return (
+            f"Pendulum at {self.location} with ball radius {self.ball_radius}"
+        )
+
     def add_sphere(self, location, radius):
         self.sphere = bpy.ops.mesh.primitive_uv_sphere_add(
-            radius=radius, 
-            location=location
+            radius=radius, location=location
         )
         self.sphere = bpy.context.active_object
 
@@ -48,8 +47,7 @@ class PendulumBlender:
 
     @staticmethod
     def calculate_cylinder_pose(
-        loc1: np.ndarray, 
-        loc2: np.ndarray
+        loc1: np.ndarray, loc2: np.ndarray
     ) -> tuple[float, np.ndarray, np.ndarray]:
         # Calculate the depth of the cylinder
         length = np.linalg.norm(loc1 - loc2)
@@ -58,66 +56,70 @@ class PendulumBlender:
         # Calculate the difference in z, y, and x
         delta = loc2 - loc1
         # Calculate the angles of the cylinder
-        angle = np.array([
-            0, 
-            np.arccos(delta[2] / length), 
-            np.arctan2(delta[1], delta[0])
-        ])
+        angle = np.array(
+            [0, np.arccos(delta[2] / length), np.arctan2(delta[1], delta[0])]
+        )
         return center, angle, length
-    
+
     def update(self, position):
         self.sphere.location = position
         center, angle, length = PendulumBlender.calculate_cylinder_pose(
-            loc1=np.array([0, 0, 0]), 
-            loc2=position
+            loc1=np.array([0, 0, 0]), loc2=position
         )
         self.cylinder.location = center
         self.cylinder.rotation_euler = angle
         self.cylinder.scale[2] = length
+
 
 class Pendulum:
     def __init__(self, length, euler_angles):
         self.length = length
         self.euler_angles = euler_angles
         self.position = self.calculate_position()
-        self.velocity = np.array([0., 0., 0.])
-    
+        self.velocity = np.array([0.0, 0.0, 0.0])
+
     def calculate_position(self):
-        x = self.length * np.cos(self.euler_angles[1]) * np.cos(self.euler_angles[0])
-        y = self.length * np.cos(self.euler_angles[1]) * np.sin(self.euler_angles[0])
+        x = (
+            self.length
+            * np.cos(self.euler_angles[1])
+            * np.cos(self.euler_angles[0])
+        )
+        y = (
+            self.length
+            * np.cos(self.euler_angles[1])
+            * np.sin(self.euler_angles[0])
+        )
         z = self.length * np.sin(self.euler_angles[1])
         return np.array([x, y, z])
-    
+
     def get_position(self):
         return self.position
-    
+
     def update(self, dt):
         # Update the velocity
-        self.position += self.velocity * dt/2
+        self.position += self.velocity * dt / 2
         self.velocity += np.array([0, 0, -9.81]) * dt
-        self.position += self.velocity * dt/2
+        self.position += self.velocity * dt / 2
 
 
 def delete_all():
-    bpy.ops.object.select_all(action='SELECT')
+    bpy.ops.object.select_all(action="SELECT")
     bpy.ops.object.delete()
 
 
 def main():
     delete_all()
-    
+
     pendulum_length = 0.3
-    pendulum_euler_angles = np.array([0., 0.])
+    pendulum_euler_angles = np.array([0.0, 0.0])
     pendulum = Pendulum(
-        length=pendulum_length, 
-        euler_angles=pendulum_euler_angles
+        length=pendulum_length, euler_angles=pendulum_euler_angles
     )
     pendulum_blender = PendulumBlender(
-        location=pendulum.get_position(), 
-        ball_radius=0.2
+        location=pendulum.get_position(), ball_radius=0.2
     )
-    
-    dt = 10**(-3)
+
+    dt = 10 ** (-3)
     framerate = 25
     simulation_ratio = int(1 / framerate / dt)
     time = np.arange(0, 10, dt)
@@ -127,16 +129,17 @@ def main():
         if k % simulation_ratio == 0:
             # Update the location of the pendulum
             pendulum_blender.update(position=pendulum.get_position())
-            
 
             # # Update the scene
             bpy.context.view_layer.update()
-    
+
     ### Saving the file ###
     write_filepath = "pendulum.blend"
-    write_filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), write_filepath)
-    #Saves above script as .blend file; stores on personal device using filepath
-    bpy.ops.wm.save_as_mainfile(filepath=write_filepath)  
+    write_filepath = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), write_filepath
+    )
+    # Saves above script as .blend file; stores on personal device using filepath
+    bpy.ops.wm.save_as_mainfile(filepath=write_filepath)
     with bpy.data.libraries.load(load_filepath, link=True) as (
         data_from,
         data_to,
@@ -144,7 +147,6 @@ def main():
         data_to.objects = [
             name for name in data_from.objects if name.startswith("S")
         ]
-
 
     # load_filepath = "Blender/3rodbr2.blend"
 
@@ -160,8 +162,6 @@ def main():
     # write_filepath = "/Blender/3rodbr2_write.blend"
     # bpy.data.libraries.write(write_filepath, set(bpy.context.selected_objects), path_remap="RELATIVE")
 
-    
-    
     # TODO: The writing part is not working
     # data_to.objects["Cube"].select_set(True)
     write_filepath = "/Blender/3rodbr2_write.blend"
