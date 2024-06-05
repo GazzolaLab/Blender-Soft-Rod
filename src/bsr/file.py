34e719c4a8f7e049ab1ea4e256a46ea868ca96b5
Rohar10 <rohith3@illinois.edu>
2024-06-04 19:32:34 -0500

    Docstring complete for reload function

--- src/bsr/file.py
+++ src/bsr/file.py
@@ -13,10 +13,10 @@ def save(path: Path | str) -> bool:
 
     To use this function, pass in a filepath in either Path or string format.
     If the input is a string, the function will convert it back into a path in a conditional.
-    Once all input types are in path format, they are converted back into a string.
+    If the input is not a Path or string, a TypeError will be raised.
+    Once all valid input types are in path format, they are converted back into a string.
     These are then used as the filepath parameter for the save_as_mainfile operator.
     This operator saves a .blend file to the filepath that is passed in.
-
 
     Parameters
     ----------
@@ -75,6 +75,55 @@ def save(path: Path | str) -> bool:
 def reload(path: Path | str) -> bool:
     """
     Reloads (resets) the file to previous save condition, and raises error if filename is not found.
+
+    To use this function, pass in a filepath in either Path or string format.
+    If the input is a string, the function will convert it back into a path in a conditional.
+    If the input is not a Path or string, a TypeError will be raised.
+    If the filepath does not exist, a FileNotFoundError will be raised.
+    If these conditions are all passed, the file is reloaded with the revert_mainfile operator.
+
+    Parameters
+    ----------
+    path: Path | str
+        File path to save the file to.
+        This can be passed in as a Path object from the pathlib library,
+        or a string literal.
+
+    Returns
+    -------
+    bool:
+        Whether the file was successfully reloaded.
+        In this case doesn't need to return anything.
+
+    Raises
+    ------
+    TypeError
+        If the type of the path parameter is not a Path or string
+
+    FileNotFoundError
+        If the passed-in filepath does not exist
+
+    Examples
+    --------
+    >>> path = /Users/rohitharish/Downloads/test_path
+    >>> bpy.ops.wm.revert_mainfile()
+    #Reloads file to last saved state
+
+    >>> path = "/Users/rohitharish/Downloads/test_path"
+    >>> path = Path(path)
+    /Users/rohitharish/Downloads/test_path
+    >>> bpy.ops.wm.revert_meainfile()
+    #Reloads file to last saved state
+
+    >>> path = 50
+    >>> raise TypeError(
+            f"Type of path should be either Path or str. Given: {type(path)}"
+        )
+    Type of path should be either Path or str. Given: {int(path)}
+
+    >>> path = /Users/rohitharish/Downloads/non_existent_file.blend
+    >>> raise FileNotFoundError("This file does not exist")
+    This file does not exist
 
     """
     if isinstance(path, Path):
