62c9b734157001b803883999892e11b708827978
Seung Hyun Kim <skim449@illinois.edu>
2024-06-07 07:44:42 +0900

    feat: add L1 and L2 error calculation in example

--- examples/single_rigid_rod_spring_action.py
+++ examples/single_rigid_rod_spring_action.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 import bsr
@@ -15,6 +16,11 @@ def f(x):
 
 def euler_forward_step(x, f, dt):
     return x + f(x) * dt
+
+
+def analytical_solution(t, y0, v0):
+    omega = np.sqrt(k)
+    return y0 * np.cos(omega * t) + v0 / omega * np.sin(omega * t)
 
 
 # Rod configuration
@@ -41,6 +47,7 @@ dt = 10 ** (-3)
 framerate = 25
 simulation_ratio = int(1 / framerate / dt)
 time = np.arange(0, 10, dt)
+analytical_solution = analytical_solution(time, position[2], velocity)
 
 # Euler-Forward time stepper
 for time_index, t in enumerate(time[:-1]):
@@ -53,3 +60,14 @@ for time_index, t in enumerate(time[:-1]):
         rod.update(keyframe=keyframe, positions=position, radius=radius)
 
 bsr.save("single_rigid_rod_spring_action.blend")
+
+# Plot L1 and L2 error
+l1_error = np.abs(analytical_solution - position[2]).sum(axis=0)
+l2_error = np.sqrt(np.square(analytical_solution - position[2]).sum(axis=0))
+plt.plot(velocity, l1_error, label="L1 error")
+plt.plot(velocity, l2_error, label="L2 error")
+plt.xlabel("Initial velocity")
+plt.ylabel("Error")
+plt.legend()
+plt.grid(True)
+plt.savefig("single_rigid_rod_spring_action_error.png", dpi=300)
