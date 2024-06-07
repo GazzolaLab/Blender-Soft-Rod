72ddffdb4274e78d662d866aa1a7f50902cb9eb4
Seung Hyun Kim <skim449@illinois.edu>
2024-06-08 00:12:28 +0900

    :fire: fix: bpy object load error in test

--- tests/test_file_handling.py
+++ tests/test_file_handling.py
@@ -4,9 +4,6 @@ import bpy
 import numpy as np
 import pytest
 
-# Had to do an extra import (pip install mathutils in Terminal); Reason shown later
-from mathutils import Vector
-
 
 # Test file creation
 def test_file_create_using_bpy(tmp_path):
@@ -15,30 +12,32 @@ def test_file_create_using_bpy(tmp_path):
     assert blend_file.exists()
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def blend_file(tmp_path_factory):
-    blend_file_path = tmp_path_factory.mktemp("data") / "test.blend"
-    bpy.ops.mesh.primitive_uv_sphere_add(radius=0.1, location=(0, 0, 0))
-    # Moved save_as_mainfile line below creation of sphere; Previously rendered empty .blend file
+    blend_file_path = (
+        tmp_path_factory.mktemp("data") / "test_file_handling.blend"
+    )
+    bpy.ops.mesh.primitive_uv_sphere_add(radius=1.0, location=(1, 2, 3))
+    obj = bpy.context.active_object
+    obj.scale[0] = 0.1
+    obj.scale.y = 0.2
+    obj.scale.z = 0.3
     bpy.ops.wm.save_as_mainfile(filepath=str(blend_file_path))
     return blend_file_path
 
 
 def test_file_opening_using_bpy(blend_file):
     bpy.ops.wm.open_mainfile(filepath=str(blend_file))
-    objects = bpy.context.scene.objects
-    loaded_object = objects.get("Sphere")
-    loaded_object_dimensions = loaded_object.dimensions
-    # The approximation is close but not quite correct
-    # 0.09999993443489075 vs 0.1
-    loaded_object_radius = loaded_object_dimensions[0] / 2
-    # Location is displayed in the form Vector((0,0,0)), which is in Mathutils package
+
+    loaded_object = bpy.context.active_object
+    np.testing.assert_allclose(loaded_object.scale.x, 0.1)
+    np.testing.assert_allclose(loaded_object.scale.y, 0.2)
+    np.testing.assert_allclose(loaded_object.scale.z, 0.3)
+
     loaded_object_location = loaded_object.location
-    exact_radius = 0.1
-    # Assert statement adjusted
-    np.testing.assert_allclose(loaded_object_radius, exact_radius, rtol=1e-6)
-    # Assert statement changed from Tuple to Vector to match Blender format
-    assert loaded_object_location == Vector((0, 0, 0))
+    np.testing.assert_allclose(loaded_object_location.x, 1.0)
+    np.testing.assert_allclose(loaded_object_location.y, 2.0)
+    np.testing.assert_allclose(loaded_object_location.z, 3.0)
 
 
 def test_file_opening_and_writing_data_using_bpy(blend_file):
@@ -46,8 +45,8 @@ def test_file_opening_and_writing_data_using_bpy(blend_file):
 
     new_radius = 0.2
     # - Note; cannot driectly change sphere radius post-definition; can work around by scaling it
-    objects = bpy.context.scene.objects
-    loaded_object = objects.get("Sphere")
+    loaded_object = bpy.context.active_object
+
     scale_factor = 2
     loaded_object.scale = (
         scale_factor * loaded_object.scale[0],
@@ -56,19 +55,22 @@ def test_file_opening_and_writing_data_using_bpy(blend_file):
     )
     # This effectively scales in each axis by the scale factor (2), increasing radius to 0.2
 
-    new_location = Vector((1, 1, 1))
-    loaded_object.location = new_location
+    loaded_object.location = (4, 5, 6)
 
     new_blend_file = blend_file.parent / "testname"
     bpy.ops.wm.save_as_mainfile(filepath=str(new_blend_file))
     assert new_blend_file.exists()
 
-    bpy.ops.wm.save_as_mainfile(filepath=str(new_blend_file))
-    loaded_object_dimensions = loaded_object.dimensions
-    loaded_object_radius = loaded_object_dimensions[0] / 2
+    bpy.ops.wm.open_mainfile(filepath=str(new_blend_file))
+    loaded_object = bpy.context.active_object
+    np.testing.assert_allclose(loaded_object.scale.x, 0.2, rtol=1e-6)
+    np.testing.assert_allclose(loaded_object.scale.y, 0.4, rtol=1e-6)
+    np.testing.assert_allclose(loaded_object.scale.z, 0.6, rtol=1e-6)
+
     loaded_object_location = loaded_object.location
-    np.testing.assert_allclose(loaded_object_radius, new_radius, rtol=1e-6)
-    assert loaded_object_location == new_location
+    np.testing.assert_allclose(loaded_object_location.x, 4)
+    np.testing.assert_allclose(loaded_object_location.y, 5)
+    np.testing.assert_allclose(loaded_object_location.z, 6)
 
 
 def test_file_saving_using_bsr_save(tmp_path):
@@ -95,7 +97,6 @@ def test_file_reload_using_bsr_reload(blend_file):
 
     # Change the radius and location of the object
     new_radius = 0.2
-    new_location = Vector((1, 1, 1))
     obj = bpy.context.active_object
     scale_factor = 2
     # This scales the radius by 2; becomes 0.2
@@ -104,7 +105,7 @@ def test_file_reload_using_bsr_reload(blend_file):
         scale_factor * obj.scale[1],
         scale_factor * obj.scale[2],
     )
-    obj.location = new_location
+    obj.location = (1, 1, 1)
 
     # Reload/Revert the saved file
     reload(saved_blend_file)
@@ -113,7 +114,9 @@ def test_file_reload_using_bsr_reload(blend_file):
     # Radius and location of the object should be the same as the original file
     obj = bpy.context.active_object
     np.testing.assert_allclose(obj.dimensions[0] / 2, 0.1, rtol=1e-6)
-    assert obj.location == Vector((0, 0, 0))
+    assert obj.location.x == 1
+    assert obj.location.y == 2
+    assert obj.location.z == 3
 
 
 def test_file_not_found_reload():
