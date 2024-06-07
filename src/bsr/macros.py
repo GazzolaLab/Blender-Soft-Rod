0d2062b7e03a4f4220d6047053d1cac915476dff
Seung Hyun Kim <skim449@illinois.edu>
2024-06-08 08:38:24 +0900

    test: add geometry shape checker

--- src/bsr/macros.py
+++ src/bsr/macros.py
@@ -8,3 +8,13 @@ def clear_mesh_objects() -> None:
     bpy.ops.object.select_all(action="DESELECT")
     bpy.ops.object.select_by_type(type="MESH")
     bpy.ops.object.delete()
+
+
+def scene_update() -> None:
+    """
+    Update the scene
+
+    Used to update object's matrix_world after transformations
+    (https://blender.stackexchange.com/questions/27667/incorrect-matrix-world-after-transformation)
+    """
+    bpy.context.view_layer.update()
---
+++ tests/geometry/utils.py
@@ -0,0 +1,22 @@
+import bpy
+import numpy as np
+
+from bsr.geometry import BlenderMeshInterfaceProtocol
+from bsr.macros import scene_update
+
+
+def get_mesh_limit(interface: BlenderMeshInterfaceProtocol):
+    """(For testing) Given blender mesh object, return xyz limit"""
+
+    obj = interface.object
+    scene_update()
+
+    vertices_coords = []
+    for v in obj.data.vertices:
+        global_coord = obj.matrix_world @ v.co
+        vertices_coords.append(list(global_coord))
+    vertices_coords = np.array(vertices_coords)
+
+    x_min, y_min, z_min = np.min(vertices_coords, axis=0)
+    x_max, y_max, z_max = np.max(vertices_coords, axis=0)
+    return x_min, x_max, y_min, y_max, z_min, z_max
