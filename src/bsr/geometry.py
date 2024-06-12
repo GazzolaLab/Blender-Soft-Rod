d1f4599452ecd9279a315b98c3daffdfc9c5523f
Seung Hyun Kim <skim449@illinois.edu>
2024-06-12 10:30:35 +0900

    refactor: single-implementation for cylinder orientation function

--- src/bsr/geometry.py
+++ src/bsr/geometry.py
@@ -13,6 +13,19 @@ from .mixin import KeyFrameControlMixin
 from .protocol import BlenderMeshInterfaceProtocol, MeshDataType
 
 
+# TODO: use numba
+def calculate_cylinder_orientation(position_1, position_2):
+    depth = np.linalg.norm(position_2 - position_1)
+    dz = position_2[2] - position_1[2]
+    dy = position_2[1] - position_1[1]
+    dx = position_2[0] - position_1[0]
+    center = (position_1 + position_2) / 2
+    phi = np.arctan2(dy, dx)
+    theta = np.arccos(dz / depth)
+    angles = np.array([phi, theta])
+    return depth, center, angles
+
+
 class Sphere(KeyFrameControlMixin):
     """
     This class provides a mesh interface for Blender Sphere objects.
@@ -108,7 +121,7 @@ class Cylinder(KeyFrameControlMixin):
         return self._obj
 
     def update_states(self, position_1, position_2, radius):
-        depth, center, angles = self.calc_cyl_orientation(
+        depth, center, angles = calculate_cylinder_orientation(
             position_1, position_2
         )
         self.object.location = center
@@ -116,19 +129,6 @@ class Cylinder(KeyFrameControlMixin):
         self.object.scale[2] = depth
         self.object.scale[0] = radius
         self.object.scale[1] = radius
-
-    def calc_cyl_orientation(self, position_1, position_2):
-        position_1 = np.array(position_1)
-        position_2 = np.array(position_2)
-        depth = np.linalg.norm(position_2 - position_1)
-        dz = position_2[2] - position_1[2]
-        dy = position_2[1] - position_1[1]
-        dx = position_2[0] - position_1[0]
-        center = (position_1 + position_2) / 2
-        phi = np.arctan2(dy, dx)
-        theta = np.arccos(dz / depth)
-        angles = np.array([phi, theta])
-        return depth, center, angles
 
     def _create_cylinder(
         self,
@@ -139,7 +139,7 @@ class Cylinder(KeyFrameControlMixin):
         """
         Creates a new cylinder object with the given end positions, radius, centerpoint and depth.
         """
-        depth, center, angles = self.calc_cyl_orientation(
+        depth, center, angles = calculate_cylinder_orientation(
             position_1, position_2
         )
         bpy.ops.mesh.primitive_cylinder_add(radius=1.0, depth=1.0)
@@ -165,70 +165,78 @@ class Cylinder(KeyFrameControlMixin):
     #    self.mat.diffuse_color = (r, g, b, a)
 
 
-# TODO: Refactor
 class Frustum(KeyFrameControlMixin):
-    def __init__(self, pos1, pos2, radius1, radius2):
-        self.obj = self.create_frustum(pos1, pos2, radius1, radius2)
-        self.mat = bpy.data.materials.new(name="cyl_mat")
-        self.obj.active_material = self.mat
+    """
+    This class provides a mesh interface for Blender Frustum objects.
+    Frustum objects are created with the given positions and radii.
 
-    def create_frustum(self, pos1, pos2, radius1, radius2):
-        depth, center, angles = self.calc_frust_orientation(pos1, pos2)
+    Parameters
+    ----------
+    position_1 : NDArray
+        The position of the first end of the frustum object. (3D)
+    position_2 : NDArray
+        The position of the second end of the frustum object. (3D)
+    radius_1 : float
+        The radius of the first end of the frustum object.
+    radius_2 : float
+        The radius of the second end of the frustum object.
+    """
+
+    def __init__(
+        self,
+        position_1: NDArray,
+        position_2: NDArray,
+        radius_1: float,
+        radius_2: float,
+    ):
+        self._obj = self._create_frustum(
+            position_1, position_2, radius_1, radius_2
+        )
+        self.update_states(position_1, position_2, radius_1, radius_2)
+
+        # self.mat = bpy.data.materials.new(name="cyl_mat")
+        # self.obj.active_material = self.mat
+
+    def _create_frustum(self, position_1, position_2, radius_1, radius_2):
+        depth, center, angles = calculate_cylinder_orientation(
+            position_1, position_2
+        )
         bpy.ops.mesh.primitive_cone_add(
-            radius1=radius1, radius2=radius2, depth=1, location=center
+            radius_1=radius_1, radius_2=radius_2, depth=1, location=center
         )
         frustum = bpy.context.active_object
         frustum.rotation_euler = (0, angles[1], angles[0])
         frustum.scale[2] = depth
         return frustum
 
-    def calc_frust_orientation(self, pos1, pos2):
-        pos1 = np.array(pos1)
-        pos2 = np.array(pos2)
-        depth = np.linalg.norm(pos2 - pos1)
-        dz = pos2[2] - pos1[2]
-        dy = pos2[1] - pos1[1]
-        dx = pos2[0] - pos1[0]
-        center = (pos1 + pos2) / 2
-        phi = np.arctan2(dy, dx)
-        theta = np.arccos(dz / depth)
-        angles = np.array([phi, theta])
-        return depth, center, angles
-
-    def update_position(self, pos1, pos2):
-        depth, center, angles = self.calc_frust_orientation(pos1, pos2)
+    def update_position(self, position_1, position_2):
+        depth, center, angles = calculate_cylinder_orientation(
+            position_1, position_2
+        )
         self.obj.location = (center[0], center[1], center[2])
         self.obj.rotation_euler = (0, angles[1], angles[0])
         self.obj.scale[2] = depth
 
-        # computing deformation heat-map
-        max_def = 0.07
-
-        h = (
-            -np.sqrt(self.obj.location[0] ** 2 + self.obj.location[2] ** 2)
-            / max_def
-            + 240 / 360
-        )
-        v = (
-            np.sqrt(self.obj.location[0] ** 2 + self.obj.location[2] ** 2)
-            / max_def
-            * 0.5
-            + 0.5
-        )
-
-        r, g, b = colorsys.hsv_to_rgb(h, 1, v)
         self.update_color(r, g, b, 1)
 
-    def update_color(self, r, g, b, a):
-        self.mat.diffuse_color = (r, g, b, a)
+    def update_states(self, position_1, position_2, radius_1, radius_2):
+        self.update_position(position_1, position_2)
 
-    def update(self, pos1, pos2, time_step):
-        self.update_position(pos1, pos2)
-        # adding to keyframe
-        self.obj.keyframe_insert(data_path="location", frame=time_step)
-        self.obj.keyframe_insert(data_path="rotation_euler", frame=time_step)
-        self.obj.keyframe_insert(data_path="scale", frame=time_step)
-        self.mat.keyframe_insert(data_path="diffuse_color", frame=time_step)
+    def set_keyframe(self, keyframe: int) -> None:
+        self.obj.keyframe_insert(data_path="location", frame=keyframe)
+        self.obj.keyframe_insert(data_path="rotation_euler", frame=keyframe)
+        self.obj.keyframe_insert(data_path="scale", frame=keyframe)
+        # self.mat.keyframe_insert(data_path="diffuse_color", frame=time_step)
+
+    # def update_color(self, val):
+    #    # computing deformation heat-map
+    #    max_def = 0.07
+
+    #    h = -np.sqrt(val) / max_def + 240 / 360
+    #    v = np.sqrt(val) / max_def * 0.5 + 0.5
+
+    #    r, g, b = colorsys.hsv_to_rgb(h, 1, v)
+    #    self.mat.diffuse_color = (r, g, b, a)
 
 
 if TYPE_CHECKING:
