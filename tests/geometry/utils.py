8fd89e3ab2fff04b871fec8983df4401407b7a3a
Rohar10 <rohith3@illinois.edu>
2024-06-17 15:39:42 -0500

    passed all test cases; awaiting documentation

--- tests/geometry/utils.py
+++ tests/geometry/utils.py
@@ -1,11 +1,12 @@
-from numbers import Number
+# from numbers import Number
 
 import bpy
 import numpy as np
-from numpy.typing import NDArray
 
 from bsr.geometry import BlenderMeshInterfaceProtocol
 from bsr.macros import scene_update
+
+# from numpy.typing import NDArray
 
 
 def get_mesh_limit(interface: BlenderMeshInterfaceProtocol):
@@ -23,17 +24,3 @@ def get_mesh_limit(interface: BlenderMeshInterfaceProtocol):
     x_min, y_min, z_min = np.min(vertices_coords, axis=0)
     x_max, y_max, z_max = np.max(vertices_coords, axis=0)
     return x_min, x_max, y_min, y_max, z_min, z_max
-
-
-def validate_position(position: NDArray) -> None:  # pragma: no cover
-    if position.shape != (3,):
-        raise ValueError("The shape of the position is incorrect.")
-    if np.isnan(position).any():
-        raise ValueError("The position contains NaN values.")
-
-
-def validate_radius(radius: float) -> None:  # pragma: no cover
-    if not isinstance(radius, Number) or radius <= 0:
-        raise ValueError("The radius must be a positive float.")
-    if np.isnan(radius):
-        raise ValueError("The radius contains NaN values.")
