84f0fd2fca43db6247c4b5ee8d0d7e8a392c9a8e
Rohar10 <rohith3@illinois.edu>
2024-06-17 15:39:42 -0500

    passed all test cases; awaiting documentation

--- src/bsr/geometry.py
+++ src/bsr/geometry.py
@@ -11,10 +11,11 @@ from numbers import Number
 import bpy
 import numpy as np
 from numpy.typing import NDArray
-from utils import validate_position, validate_radius
 
 from .mixin import KeyFrameControlMixin
 from .protocol import BlenderMeshInterfaceProtocol, MeshDataType
+
+# from utils import validate_position, validate_radius
 
 
 # TODO: use numba
@@ -32,19 +33,19 @@ def calculate_cylinder_orientation(
     return float(depth), center, angles
 
 
-# TODO: refactor into utility - DONE
-# def _validate_position(position: NDArray) -> None:  # pragma: no cover
-#    if position.shape != (3,):
-#        raise ValueError("The shape of the position is incorrect.")
-#    if np.isnan(position).any():
-#        raise ValueError("The position contains NaN values.")
+# TODO: refactor into utility - DONE; Errors came up, not sure why
+def _validate_position(position: NDArray) -> None:  # pragma: no cover
+    if position.shape != (3,):
+        raise ValueError("The shape of the position is incorrect.")
+    if np.isnan(position).any():
+        raise ValueError("The position contains NaN values.")
 
 
-# def _validate_radius(radius: float) -> None:  # pragma: no cover
-#    if not isinstance(radius, Number) or radius <= 0:
-#        raise ValueError("The radius must be a positive float.")
-#    if np.isnan(radius):
-#        raise ValueError("The radius contains NaN values.")
+def _validate_radius(radius: float) -> None:  # pragma: no cover
+    if not isinstance(radius, Number) or radius <= 0:
+        raise ValueError("The radius must be a positive float.")
+    if np.isnan(radius):
+        raise ValueError("The radius contains NaN values.")
 
 
 class Sphere(KeyFrameControlMixin):
@@ -106,12 +107,12 @@ class Sphere(KeyFrameControlMixin):
         """
 
         if position is not None:
-            validate_position(position)
+            _validate_position(position)
             self.object.location.x = position[0]
             self.object.location.y = position[1]
             self.object.location.z = position[2]
         if radius is not None:
-            validate_radius(radius)
+            _validate_radius(radius)
             self.object.scale = (radius, radius, radius)
 
     def _create_sphere(self) -> bpy.types.Object:
@@ -143,11 +144,7 @@ class Cylinder(KeyFrameControlMixin):
         position_2: NDArray,
         radius: float,
     ) -> None:
-        self._obj = self._create_cylinder(
-            position_1,
-            position_2,
-            radius,
-        )
+        self._obj = self._create_cylinder()
         # FIXME: This is a temporary solution
         # Ideally, these modules should not contain any data
         self._states = {
@@ -155,6 +152,7 @@ class Cylinder(KeyFrameControlMixin):
             "position_2": position_2,
             "radius": radius,
         }
+        self.update_states(position_1, position_2, radius)
 
     input_keys = {"position_1", "position_2", "radius"}
 
@@ -180,20 +178,22 @@ class Cylinder(KeyFrameControlMixin):
         if position_1 is None and position_2 is None and radius is None:
             return
         if position_1 is not None:
-            validate_position(position_1)
+            _validate_position(position_1)
             self._states["position_1"] = position_1
         else:
             position_1 = self._states["position_1"]
         if position_2 is not None:
-            validate_position(position_2)
+            _validate_position(position_2)
             self._states["position_2"] = position_2
         else:
             position_2 = self._states["position_2"]
         if radius is not None:
-            validate_radius(radius)
+            _validate_radius(radius)
             self._states["radius"] = radius
         else:
             radius = self._states["radius"]
+        if np.allclose(position_1, position_2):
+            raise ValueError("Endpoints are the same")
 
         depth, center, angles = calculate_cylinder_orientation(
             position_1, position_2
@@ -206,26 +206,15 @@ class Cylinder(KeyFrameControlMixin):
 
     def _create_cylinder(
         self,
-        position_1: NDArray,
-        position_2: NDArray,
-        radius: float,
     ) -> bpy.types.Object:
         """
         Creates a new cylinder object with the given end positions, radius, centerpoint and depth.
         """
-        depth, center, angles = calculate_cylinder_orientation(
-            position_1, position_2
-        )
         bpy.ops.mesh.primitive_cylinder_add(
             radius=1.0,
             depth=1.0,
         )  # Fix keep these values as default.
         cylinder = bpy.context.active_object
-        cylinder.location = center
-        cylinder.rotation_euler = (0, angles[1], angles[0])
-        cylinder.scale[2] = depth
-        cylinder.scale[1] = radius
-        cylinder.scale[0] = radius
         return cylinder
 
     def set_keyframe(self, keyframe: int) -> None:
@@ -239,17 +228,6 @@ class Cylinder(KeyFrameControlMixin):
         self.object.keyframe_insert(data_path="location", frame=keyframe)
         self.object.keyframe_insert(data_path="rotation_euler", frame=keyframe)
         self.object.keyframe_insert(data_path="scale", frame=keyframe)
-        # self.object.keyframe_insert(data_path="diffuse_color", frame=keyframe)
-
-    # def update_color(self, val):
-    #    # computing deformation heat-map
-    #    max_def = 0.07
-
-    #    h = -np.sqrt(val) / max_def + 240 / 360
-    #    v = np.sqrt(val) / max_def * 0.5 + 0.5
-
-    #    r, g, b = colorsys.hsv_to_rgb(h, 1, v)
-    #    self.mat.diffuse_color = (r, g, b, a)
 
 
 # TODO: Will be implemented in the future
