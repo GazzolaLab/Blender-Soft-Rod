1d41ebd27708afc885db6e07a7a6d3768dedc8d3
Seung Hyun Kim <skim449@illinois.edu>
2024-06-21 12:59:01 -0500

    fix: elastica callback stops with nan detection

--- src/elastica_blender/rod_callback.py
+++ src/elastica_blender/rod_callback.py
@@ -14,24 +14,29 @@ class BlenderRodCallback(CallBackBaseClass):
     PyElastica callback to save rod state to Blender.
     """
 
-    def __init__(self, step_skip: int) -> None:
+    def __init__(self, step_skip: int, *args, **kwargs) -> None:
         CallBackBaseClass.__init__(self)
         self.every = step_skip
         self.keyframe = 0
         self.bpy_objs: bsr.Rod
+        self.stop = False
 
     def make_callback(
         self, system: RodType, time: np.floating, current_step: int
     ) -> None:
-        if current_step % self.every == 0:
-            if current_step == 0:
-                self.bpy_objs = bsr.Rod(
-                    system.position_collection, system.radius
-                )
-            else:
-                self.bpy_objs.update_states(
-                    positions=system.position_collection,
-                    radii=system.radius,
-                )
-            self.bpy_objs.set_keyframe(self.keyframe)
-            self.keyframe += 1
+        if self.stop or current_step % self.every != 0:
+            return
+        if np.isnan(system.position_collection).any() or np.isnan(system.radius).any():
+            self.stop = True
+            return
+        if current_step == 0:
+            self.bpy_objs = bsr.Rod(
+                system.position_collection, system.radius
+            )
+        else:
+            self.bpy_objs.update_states(
+                positions=system.position_collection,
+                radii=system.radius,
+            )
+        self.bpy_objs.set_keyframe(self.keyframe)
+        self.keyframe += 1
