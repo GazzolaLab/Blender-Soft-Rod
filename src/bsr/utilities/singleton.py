a018589d44fc34bd3a766d9374ede9600377004b
Seung Hyun Kim <skim449@illinois.edu>
2024-08-12 15:55:25 -0500

    mypy fix

--- src/bsr/utilities/singleton.py
+++ src/bsr/utilities/singleton.py
@@ -1,5 +1,8 @@
+from typing_extensions import Self
+
+
 class Singleton:
-    def __new__(cls):
+    def __new__(cls) -> Self:
         if not hasattr(cls, "_instance"):
             cls._instance = super(Singleton, cls).__new__(cls)
-        return cls._instance
+        return cls._instance  # type: ignore
