bc7bd932f9371012d160406ceafa7dc349421717
Hanson-HSChang <hanson.hschang@gmail.com>
2024-08-12 16:59:17 -0500

    wip: fix type annotation

--- src/bsr/utilities/singleton.py
+++ src/bsr/utilities/singleton.py
@@ -4,11 +4,11 @@ from typing_extensions import Self
 class Singleton:
     __instance = None
 
-    def __new__(cls):
+    def __new__(cls):  # type: ignore
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)
-        return cls.__instance  # type: ignore
+        return cls.__instance
 
     @property
-    def isInstantiated(self):
+    def isInstantiated(self) -> bool:
         return self.__instance is not None
