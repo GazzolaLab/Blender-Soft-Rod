9c0ee37d11c79e5a428d02b0e309ac7e8ff73488
Heng-Sheng Chang <38339385+hanson-hschang@users.noreply.github.com>
2024-08-21 12:11:49 -0500

    Update singleton.py

--- src/bsr/utilities/singleton.py
+++ src/bsr/utilities/singleton.py
@@ -3,5 +3,5 @@ class SingletonMeta(type):
 
     def __call__(cls, *args, **kwargs):  # type: ignore
         if cls not in cls.__instance:
-            cls.__instance[cls] = super().__call__(*args, **kwargs)
+            cls.__instance[cls] = super(SingletonMeta, cls).__call__(*args, **kwargs)
         return cls.__instance[cls]
