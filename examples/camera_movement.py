24dd25f57bc887087e7d20c07ac792fe0f6b47b5
Hanson-HSChang <hanson.hschang@gmail.com>
2024-09-18 15:29:59 -0500

    wip: update example file: camera_movement.py

--- examples/camera_movement.py
+++ examples/camera_movement.py
@@ -4,25 +4,29 @@ import bsr
 from bsr.geometry.composite.pose import Pose
 
 
-def main(filename: str = "camera_movement"):
+def main(
+    filename: str = "camera_movement",
+    frame_rate: int = 60,
+    total_time: float = 5.0,
+    camera_location_height: float = 1.0,
+    camera_orbiting_radius: float = 1.0,
+):
 
-    frame_rate = 60
-    total_time = 5
-
-    camera_heigh = 1.0
-    camera_radius = 1.0
-
+    # Create a new scene
     bsr.clear_mesh_objects()
 
-    bsr.camera.look_at = np.array([0.0, 0.0, 0.0])
+    # Set the camera film background to transparent
     bsr.camera.set_film_transparent()
 
-    pose = Pose(
+    # Set the camera look at location
+    bsr.camera.look_at = np.array([0.0, 0.0, 0.0])
+
+    # Set a frame at the origin
+    _ = Pose(
         position=np.zeros(3),
         directors=np.identity(3),
         unit_length=0.25,
     )
-    angles = np.linspace(0.0, 360.0, frame_rate * total_time, endpoint=False)
 
     # Set the initial keyframe number
     bsr.frame_manager.set_frame_start(0)
@@ -30,15 +34,22 @@ def main(filename: str = "camera_movement"):
     # Set the current frame number
     bsr.frame_manager.frame_current = 0
 
+    # Set the camera orbiting keyframes
+    angles = np.linspace(0.0, 360.0, frame_rate * total_time, endpoint=False)
     for k, angle in enumerate(angles):
+
+        # Set the camera location
         bsr.camera.location = np.array(
             [
-                camera_radius * np.cos(np.radians(angle)),
-                camera_radius * np.sin(np.radians(angle)),
-                camera_heigh,
+                camera_orbiting_radius * np.cos(np.radians(angle)),
+                camera_orbiting_radius * np.sin(np.radians(angle)),
+                camera_location_height,
             ]
         )
+
+        # Update the keyframe
         bsr.camera.set_keyframe(bsr.frame_manager.frame_current)
+
         if k != len(angles) - 1:
             # Update the keyframe number
             bsr.frame_manager.update()
