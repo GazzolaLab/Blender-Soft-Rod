3c429b98bfa87180bc90843dd4e423b8a2414b5a
Hanson-HSChang <hanson.hschang@gmail.com>
2024-10-18 11:55:39 -0500

    wip: update pose_demo and fix code style

--- examples/pose_demo.py
+++ examples/pose_demo.py
@@ -2,11 +2,24 @@ import numpy as np
 
 import bsr
 from bsr import Pose
-import bpy
 
 
 def angle_to_color(angle: float) -> np.ndarray:
-    # Normalize angle to 0-360 range
+    """
+    Convert angle to RGB color value
+
+    Parameters
+    ----------
+    angle : float
+        The angle in degrees
+
+    Returns
+    -------
+    np.ndarray
+        The RGBA color value
+    """
+
+    # Reset angle range to 0-360 degrees
     angle = angle % 360
 
     # Convert angle to radians
@@ -14,70 +27,85 @@ def angle_to_color(angle: float) -> np.ndarray:
 
     # Calculate RGB values
     if angle < 120:
-        r = np.cos(rad) / 2 + 0.5
-        g = np.sin(rad) / 2 + 0.5
-        b = 0
+        rad = 3 * rad / 2
+        r = 0.5 + np.sin(rad) / 2
+        g = 0.5 - np.sin(rad) / 2
+        b = 0.5 - np.sin(rad) / 2
     elif angle < 240:
-        r = 0
-        g = np.cos(rad - 2 * np.pi / 3) / 2 + 0.5
-        b = np.sin(rad - 2 * np.pi / 3) / 2 + 0.5
+        rad = rad - 2 * np.pi / 3
+        rad = 3 * rad / 2
+        r = 0.5 - np.sin(rad) / 2
+        g = 0.5 + np.sin(rad) / 2
+        b = 0.5 - np.sin(rad) / 2
     else:
-        r = np.sin(rad - 4 * np.pi / 3) / 2 + 0.5
-        g = 0
-        b = np.cos(rad - 4 * np.pi / 3) / 2 + 0.5
+        rad = rad - 2 * np.pi / 3 * 2
+        rad = 3 * rad / 2
+        r = 0.5 - np.sin(rad) / 2
+        g = 0.5 - np.sin(rad) / 2
+        b = 0.5 + np.sin(rad) / 2
 
     # Return RGBA numpy array
     return np.array([r, g, b, 1.0])
 
 
-def main(filename: str = "pose_demo5"):
+def main(filename: str = "pose_demo"):
 
-    # initial values for frame rate and period
+    # initial values for frame rate and total time
     frame_rate = 60
     total_time = 5
 
     # calculates total number of frames in the visualization
     total_frames = frame_rate * total_time
 
-
     # clears all mesh objects
     bsr.clear_mesh_objects()
-    bsr.frame_manager.set_frame_start()
-    
-    # intializes pose instance and angle
-    pose_object = Pose(positions=np.array([1, 0, 0]), directors=np.eye(3),thickness_ratio=0.1)
-    theta = 0
 
-    # iterates through each frame in total time duration
-    for frame in range(total_frames):
-        theta = 2*np.pi*frame/total_frames
+    # initializes pose instance
+    pose_object = Pose(
+        positions=np.array([1, 0, 0]),
+        directors=np.eye(3),
+        thickness_ratio=0.1,
+    )
+
+    # creates an array of angles from 0 to 360 degrees
+    angles = np.linspace(0, 360, total_frames)
+
+    # Set frame start
+    bsr.frame_manager.set_frame_start()
+
+    # iterates through each angle
+    for angle in angles:
 
         # defines path of of motion for positions of pose object
-        new_positions = np.array([np.cos(theta), np.sin(theta), 0])
-        pose_object.positions = new_positions
+        positions = [np.cos(np.radians(angle)), np.sin(np.radians(angle)), 0.0]
 
         # defines directors of pose object
-        d2 = np.array([-np.sin(theta), np.cos(theta), 0])
-        d3 = np.array([0, 0, 1])
+        d2 = [-np.sin(np.radians(angle)), np.cos(np.radians(angle)), 0.0]
+        d3 = [0, 0, 1]
         d1 = np.cross(d2, d3)
-        new_directors = np.column_stack((d1, d2, d3))
-        pose_object.directors = new_directors
+        directors = np.column_stack((d1, d2, d3))
 
         # updates positions and directors of pose object at each keyframe
-        pose_object.update_states(new_positions, new_directors)
+        pose_object.update_states(
+            positions=np.array(positions),
+            directors=directors,
+        )
 
         # converts angle to rgb color value at each frame
-        color = angle_to_color(np.degrees(theta))
+        color = angle_to_color(angle)
 
         # updates pose object's colors
-        pose_object.update_material(color = color)
+        pose_object.update_material(color=color)
 
         # sets and updates keyframes
-        pose_object.set_keyframe(frame)
-        bsr.frame_manager.update()
-    
-    # Set the final keyframe number
-    bsr.frame_manager.set_frame_end()
+        pose_object.set_keyframe(bsr.frame_manager.frame_current)
+
+        if bsr.frame_manager.frame_current == total_frames - 1:
+            # Set the final keyframe
+            bsr.frame_manager.set_frame_end()
+        else:
+            # updates frame
+            bsr.frame_manager.update()
 
     # Set the frame rate
     bsr.frame_manager.set_frame_rate(fps=frame_rate)
@@ -85,7 +113,7 @@ def main(filename: str = "pose_demo5"):
     # Set the view distance
     bsr.set_view_distance(distance=5)
 
-    # Deslect all objects
+    # Deselect all objects
     bsr.deselect_all()
 
     # Select the camera object
