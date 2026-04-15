da219335fef04fcbe8ee8b0179c2567c92ec46e1
Ali-7800 <alibaz7800@gmail.com>
2026-04-15 15:34:42 -0500

    added RodMeshSurfaceContactGridMethodWithAnisotropicFriction to __init__

--- src/virtual_field/runtime/custom_elastica/mesh/__init__.py
+++ src/virtual_field/runtime/custom_elastica/mesh/__init__.py
@@ -1,5 +1,5 @@
 from .mesh_surface import MeshSurface
 from .mesh_contact_utils import Grid
-from .rod_mesh_surface_contact import RodMeshSurfaceContactGridMethod
+from .rod_mesh_surface_contact import RodMeshSurfaceContactGridMethod,RodMeshSurfaceContactGridMethodWithAnisotropicFriction
 
-__all__ = ["MeshSurface", "Grid", "RodMeshSurfaceContactGridMethod"]
+__all__ = ["MeshSurface", "Grid", "RodMeshSurfaceContactGridMethod","RodMeshSurfaceContactGridMethodWithAnisotropicFriction"]
