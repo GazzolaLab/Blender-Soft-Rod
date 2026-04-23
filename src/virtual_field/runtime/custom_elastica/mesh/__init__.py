dde9b5775781f78c2f28ea9e511e635208acf212
Ali-7800 <alibaz7800@gmail.com>
2026-04-23 14:51:24 -0500

    add SphereMeshSurfaceContact to __init__.py

--- src/virtual_field/runtime/custom_elastica/mesh/__init__.py
+++ src/virtual_field/runtime/custom_elastica/mesh/__init__.py
@@ -1,5 +1,10 @@
 from .mesh_surface import MeshSurface
 from .mesh_contact_utils import Grid
 from .rod_mesh_surface_contact import RodMeshSurfaceContactGridMethod,RodMeshSurfaceContactGridMethodWithAnisotropicFriction
-
-__all__ = ["MeshSurface", "Grid", "RodMeshSurfaceContactGridMethod","RodMeshSurfaceContactGridMethodWithAnisotropicFriction"]
+from .sphere_mesh_surface_contact import SphereMeshSurfaceContact 
+__all__ = [
+    "MeshSurface",
+    "Grid",
+    "RodMeshSurfaceContactGridMethod",
+    "RodMeshSurfaceContactGridMethodWithAnisotropicFriction",
+    "SphereMeshSurfaceContact"]
